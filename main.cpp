ddebbf5815044cad4e1e84cf2c00899a9abfbe9f
JacobLRe27 <1234@hotmail.com>
2026-03-31 10:05:02 -0600

    made the main random code and a bit of the user interface

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <random>
+#include <ctime>
+using namespace std;
+
+int random3digit(int &x, int &y, int &z);
+
+
 
 int main()
 {
+    int secretCode1 = 0, secretCode2 = 0, secretCode3 = 0;
+    random3digit(secretCode1, secretCode2, secretCode3);
+    cout << secretCode1 << secretCode2 << secretCode3 << endl;
+
+    cout<<"Welcome to Mastermind!">> endl;
+
+
+
+
+    return 0;
+}
+
+
+//
+//
+int random3digit(int &x, int &y, int &z)
+{
+    srand(time(NULL));
+    x = rand() % 10;
+    y = rand() % 10;
+    z = rand() % 10;
+
     return 0;
 
 }
